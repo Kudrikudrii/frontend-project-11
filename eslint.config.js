d5abdc89860ace7d298c4df881c7eb896950052f
Sergei Garankin <gudri@bk.ru>
2025-05-26 20:20:31 +0300

    add eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
+
+export default [
+  js.configs.recommended,
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+        ...globals.browser,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      'no-underscore-dangle': [
+        'error',
+        {
+          allow: ['__filename', '__dirname'],
+        },
+      ],
+      'import/extensions': [
+        'error',
+        {
+          js: 'always',
+        },
+      ],
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'no-console': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'object-curly-newline': 'off',
+    },
+  },
+];
