c84f07769b8596fd9965fe60090b1519a4a297c3
Sergei Garankin <gudri@bk.ru>
2025-06-07 20:08:58 +0300

    src/js/ru.js

---
+++ src/js/ru.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    errors: {
+      empty: 'Не должно быть пустым',
+      url: 'Ссылка должна быть валидным URL',
+      alreadyOnTheList: 'RSS уже существует',
+      network: 'Ошибка сети',
+      parseError: 'Ресурс не содержит валидный RSS',
+      unknown: 'Неизвестная ошибка',
+    },
+    status: {
+      success: 'RSS успешно загружен',
+    },
+    elements: {
+      feeds: 'Фиды',
+      posts: 'Посты',
+      button: 'Просмотр',
+    },
+  },
+};
