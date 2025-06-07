6d340d5016af0d0d72c7940f104f353efeba23d1
Sergei Garankin <gudri@bk.ru>
2025-06-07 20:44:03 +0300

    allorigins api

---
+++ src/js/allorigins.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const getProxyUrl = (url) => {
+  const proxyUrl = new URL('https://allorigins.hexlet.app/get');
+  proxyUrl.searchParams.set('disableCache', 'true');
+  proxyUrl.searchParams.set('url', url);
+  return proxyUrl.toString();
+};
+
+const fetchRSS = (url) => {
+  const proxyUrl = getProxyUrl(url);
+
+  return axios
+    .get(proxyUrl)
+    .then((responce) => {
+      if (responce.status !== 200) {
+        throw new Error('errors.network');
+      }
+      return responce.data.contents;
+    })
+    .catch(() => {
+      throw new Error('errors.network');
+    });
+};
+
+export default fetchRSS;
